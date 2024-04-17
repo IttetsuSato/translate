df6b7a218f0b51d21ba10e28a39eff1f8323dc07
Ittetsu Sato <ittetsusato@gmail.com>
2024-04-17 19:31:50 +0900

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { css } from "@emotion/css";
-import { useState } from "react";
 
 function App() {
-
-  return <div className={css`
-  font-size: x-large;
-  color: blue;
-  `}>
-    Hello Emotion!
-  </div>;
+  return (
+    <div
+      className={css`
+        font-size: x-large;
+        color: blue;
+      `}
+    >
+      Hello Emotion!
+    </div>
+  );
 }
 
 export default App;
